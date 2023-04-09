7121753353b49e41992625ee8ad8a2b2e01ce9eb
Yash Sain <yashsain.2406@gmail.com>
2023-04-09 09:32:12 +0530

    navbar in account page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Navigate, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import './styleAccount.css';
+import {faBus} from "@fortawesome/react-fontawesome"
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+import * as bootstrap from 'bootstrap'
+import Alert from 'bootstrap/js/dist/alert' 
 const Account = () => {
 
   const {user,logout} = UserAuth();
@@ -16,12 +23,25 @@ const Account = () => {
   }
 
   return (
-    <div>
-      Account
-    <p>User Email: {user && user.email}</p>
-    <button onClick={handleLogout}>Logout</button>
+    <div className='navbarmain'>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+        <div>
+        <i class="fa-solid fa-bus fa-xl"></i>
+        </div>
+          <div class='namemain'>
+            CityChalo
+          </div>
+          <div class="d-flex">
+            {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> */}
+            {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
+            <button class="btn btn-outline-success" onClick={handleLogout}>Logout</button>
+          </div>
+        </div>
+      </nav>
+      
     </div>
-  )
+  ) 
 }
 
 export default Account
